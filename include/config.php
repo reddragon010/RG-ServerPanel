a689429ec3a9979f9054022de862966975c76ec4
Salja <Salja@Salja.de>
2011-01-31 11:58:44 +0100

    Remove Tabs from config.php
    Signed-off-by: Salja <Salja@Salja.de>

--- include/config.php
+++ include/config.php
@@ -1,19 +1,19 @@
 <?php
-$config['db']['host']						="127.0.0.1:3306"; 	//Database Host
-$config['db']['username']				="root"; 						//Database Connect Username
-$config['db']['password']				=""; 								//Database Connect Password
+$config['db']['host']     = "127.0.0.1:3306"; // Database Host
+$config['db']['username'] = "root"; // Database Connect Username
+$config['db']['password'] = "root"; // Database Connect Password
 
-$config['db']['realmdb']				="s_realmd"; 				//AccountDatabase
-$config['db']['chardb']					="s_characters"; 		//CharactersDatabase
-$config['db']['webdb']			="s_website";
+$config['db']['realmdb'] = "realmd"; // AccountDatabase
+$config['db']['chardb']  = "characters"; // CharactersDatabase
+$config['db']['webdb']   = "website";
 
-$config['realmserver']['host']	="127.0.0.1"; 			//Your Server Realmlist
+$config['realmserver']['host'] = "127.0.0.1"; // Your Server Realmlist
 
-$config['worldserver']['ip']		="127.0.0.1";
-$config['worldserver']['port']	="3306";
-$config['worldserver']['host'] 	="localhost";
+$config['worldserver']['ip']   = "127.0.0.1";
+$config['worldserver']['port'] = "3306";
+$config['worldserver']['host'] = "localhost";
 
-$config['root_url'] 						="/website";
+$config['root_url'] = "/website";
 
 $website = "website";
 $cod = 'utf8';
