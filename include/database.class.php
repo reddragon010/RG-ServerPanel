1d71528aef89382b3aa436cb7b9853e8eff2dffc
Michael Riedmann <reddragon010@gmx.net>
2011-02-03 03:41:16 +0100

    removed debug echo --- sorry

--- include/database.class.php
+++ include/database.class.php
@@ -15,7 +15,6 @@ class Database {
   }
  
   public function query($query) {
-		echo $query;
   	$this->result=mysql_query($query,$this->connection) or die('SQL-ERROR: ' . mysql_error());
   	$this->counter=NULL;
   }
