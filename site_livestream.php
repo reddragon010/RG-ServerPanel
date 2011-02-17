967b5ddb9e18398b09ad3176dfd9eb233b773af1
Wizard <usama_morad_dev@hotmail.de>
2011-02-17 13:30:43 +0100

    Config Fixes, Function Fixes

--- site_livestream.php
+++ site_livestream.php
@@ -1,5 +1,12 @@
 <?php
 	require_once('common.php');
+	
+	$count = countLiveStreams();
+	$livestreams = getLiveStreams();
+	
 	$tpl = $twig->loadTemplate('site_livestream.tpl');
-	echo $tpl->render(array());
+	echo $tpl->render(array(
+	'count' => $count,
+	'id' => $livestreams
+	));
 ?>
