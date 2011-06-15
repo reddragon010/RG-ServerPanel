12c90f09032f32daefaf95628f71477c8688b198
Michael Riedmann <mriedmann@RRIs-MacBook.local>
2011-06-15 17:37:13 +0200

    fixed chars-index

--- app/controllers/characters_controller.php
+++ app/controllers/characters_controller.php
@@ -8,46 +8,15 @@ class CharactersController extends BaseController {
     function index($params=array()) {
         global $ALLIANCE, $HORDE;
 
-        if (isset($params['realm'])) {
-            $realm = Realm::find($params['realm']);
-        } else {
-            $realm = Realm::find(1);
+        $realms = Realm::find('all');
+        $chars = array();
+        
+        foreach($realms as $realm){
+            $chars += $realm->find_characters('all');
         }
 
-        if (isset($params['order'])) {
-            $sort_order = protect($params['order']);
-            if ($sort_order == 'ASC') {
-                $new_sort_order = 'DESC';
-            } else {
-                $new_sort_order = 'ASC';
-            }
-        } else {
-            $sort_order = 'ASC';
-            $new_sort_order = 'ASC';
-        }
-
-        if (isset($params['sort'])) {
-            $chars = $realm->find_characters('all', array('conditions' => array(''), 'sort' => protect($params['sort']) . ' ' . $sort_order));
-        } else {
-            $chars = $realm->find_characters('all', array('conditions' => array("online = '1'")));
-        }
-
-        $chars_count = $realm->find_characters_count(array('conditions' => array("online = '1'")));
-        $chars_ally_count = $realm->find_characters_count(array('conditions' => array("online = '1'", "`race` IN (" . implode(',', $ALLIANCE) . ")")));
-        $chars_horde_count = $realm->find_characters_count(array('conditions' => array("online = '1'", "`race` IN (" . implode(',', $HORDE) . ")")));
-        $gms = array_filter($chars, function($char) {
-                    return $char->user->is_gm();
-                });
-        $gms_count = count($gms);
         $tpl_data = array(
-            'chars' => $chars,
-            'chars_count' => $chars_count,
-            'ally_count' => $chars_ally_count,
-            'horde_count' => $chars_horde_count,
-            'gms' => $gms,
-            'gms_count' => $gms_count,
-            'realm' => $realm,
-            'sort_order' => $new_sort_order
+            'characters' => $chars,
         );
         $this->render($tpl_data);
     }
