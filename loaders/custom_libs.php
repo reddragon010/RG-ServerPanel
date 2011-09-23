8b5cb024fd04344447fce87282999bc752fbd140
Michael Riedmann <reddragon010@gmx.net>
2011-09-23 16:59:54 +0200

    added basic TCSoapClient-Class

--- loaders/custom_libs.php
+++ loaders/custom_libs.php
@@ -6,3 +6,5 @@ require_once(FRAMEWORK_ROOT . '/lib/SqlS/Autoloader.php');
 SqlS_Autoloader::register();
 
 require_once(FRAMEWORK_ROOT . '/lib/sfYaml/sfYaml.php');
+
+require_once(FRAMEWORK_ROOT . '/lib/trinity_soap_client.php');
---
+++ test/lib/TrinitySoapcClientTest.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * Copyright (C) 2011 Michael Riedmann
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+class TrinitySoapClientTest extends PHPUnit_Framework_TestCase {
+    private $client;
+    
+    function setUp() {
+        $config = array(
+            'host' => '127.0.0.1',
+            'port' => '7878',
+            'user' => '',
+            'pass' => ''
+        );
+        $this->client = new TrinitySoapClient($config);
+        $this->client->connect();
+    }
+/*
+    function testFetching() {
+        $result = $this->client->fetch('lookup item %s', 'Robe');
+    }
+    
+    function testLookupItems() {
+        $result = $this->client->lookup('item', 'Robe');
+        $this->assertEquals(count($result),  932);
+    }
+ */
+    function testKick() {
+        $result = $this->client->kick('Robigo');
+        echo $result;
+    }
+}
+
+?>
