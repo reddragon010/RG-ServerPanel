ff45d6ad2cb86ca1fd6073ff3b260dff7fcb2ab6
Michael Riedmann <reddragon010@gmx.net>
2012-01-20 01:22:01 +0100

    added basic arena-team index- and show-feature

---
+++ app/controllers/arena_teams_controller.php
@@ -0,0 +1,64 @@
+<?php
+/*
+ *    Copyright (C) 2011 Michael Riedmann <michael.riedmann@gmx.net>    
+ *
+ *    his file is part of RG-ServerPanel.
+ *
+ *    RG-ServerPanel is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    RG-ServerPanel is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with RG-ServerPanel.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class ArenaTeamsController extends BaseController {
+    var $before = array(
+        'check_login'
+    );
+    
+    function index($params=array()) {
+        $realms = Realm::find()->all();
+        $realmnames = array('all' => 'All');
+        foreach($realms as $r){
+            $realmnames[$r->id] = $r->name;
+        }
+        
+        $find = ArenaTeam::find()->where(array_filter($params))->page($params['page']);
+        
+        $teams = array();
+        $teams_count = 0;
+        if(isset($params['realm']) && is_numeric($params['realm'])){
+            $find = $find->realm($params['realm']);
+            $teams += $find->all();
+            $teams_count += $find->count();
+        } else {
+            $teams_count = 0;
+            foreach ($realms as $realm) {
+                $find = $find->realm($realm->id);
+                $teams += $find->all();
+                $teams_count += $find->count();
+            }
+        }
+        
+        $this->render(array(
+            'teams' => $teams,
+            'teams_count' => $teams_count,
+            'realmnames' => $realmnames
+        ));
+    }
+    
+    function show($params){
+        $team = ArenaTeam::find()->where(array('arenateamid'=> $params['id']))->realm($params['rid'])->first();
+        $data = array(
+            'team' => $team
+        );
+        $this->render($data);
+    }
+}
