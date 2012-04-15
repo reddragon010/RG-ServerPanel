ed3ae21da32bffeb7c36f759f4f7d7e58c3ce6e0
Michael Riedmann <reddragon010@gmx.net>
2012-04-15 23:00:52 +0200

    fixed little interface-definition mistake

--- core/model_interface.php
+++ core/model_interface.php
@@ -20,7 +20,7 @@
 
 interface ModelInterface {
     public static function find($pk=null);
-    public static function create($params, &$obj);
+    public static function create($params=array(), &$obj=null);
     public static function build($data, $new);
     public static function set_dbname($dbname);
     public static function set_dbid($id);
